c97f7f5d9abd3b05a8d2a68800b4c1c622f1910a
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-03-21 14:21:19 +0000

    Created page to calculate dimensions of the rectangle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Anastasiia Lebedenko
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('add-math').innerHTML = 'Perimeter = ' + (15 + 9 + 15 + 9) + 'cm'
+  document.getElementById('multiply-math').innerHTML = 'Area = ' + (15 * 9) + 'cm²'
+}
